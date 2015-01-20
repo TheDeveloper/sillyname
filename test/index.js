08dafa24d30c3a41e321df46e6925f96a786556e
Juho Vepsalainen <bebraw@gmail.com>
2015-01-20 15:36:14 +0200

    Allow random generator to be injected
    Now it is possible to inject an optional random generator to the API
    functions. The benefit of this is that you have more control over
    generation. For instance you can use an external library with a
    controllable seed for this purpose (same results always).

--- test/index.js
+++ test/index.js
@@ -1,12 +1,24 @@
 var should = require('should');
+var seedRandom = require('seed-random');
 var generateName = require('../');
 
 var silly = 'string';
 
-describe('When generating a random name', function(){
+describe('When generating a random name', suite.bind(null, generateName));
+describe('When generating a random noun', suite.bind(null, generateName.randomNoun));
+describe('When generating a random adjective', suite.bind(null, generateName.randomAdjective));
+
+function suite(fn) {
   it('should work', function(done){
-    var name = generateName();
+    var name = fn();
     name.should.be.a(silly);
     done();
   });
-});
+
+  it('should work with a generator', function(done){
+    var firstName = fn(seedRandom('random'));
+    var secondName = fn(seedRandom('random'));
+    firstName.should.equal(secondName);
+    done();
+  });
+}
