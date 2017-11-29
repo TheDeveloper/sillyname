74640144b46403777bb921b0e8f43303019937e6
earthbound19 <>
2017-11-28 18:48:09 -0700

    Add reference URL in comment

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 'use strict';
-// Attempting this via this reference: https://github.com/sindresorhus/superb/blob/master/cli.js
+// Reference: https://github.com/sindresorhus/superb/blob/master/cli.js -- http://blog.npmjs.org/post/118810260230/building-a-simple-command-line-tool-with-npm#notes :
 var generateName = require('./index.js');
 var sillyName = generateName();
 
